6becff28ad8df29db9af951f31c88c4061206723
Zolo101 <umadeguy@gmail.com>
2023-10-21 00:39:25 +0100

    Configure CORS

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,5 +5,18 @@ export const handle = (async ({ event, resolve }) => {
     const accessToken = event.cookies.get("access_token");
 
     if (accessToken) event.locals.user = await oauth.getUser(accessToken);
-    return resolve(event);
+
+    const response = await resolve(event);
+
+    if (event.url.pathname.startsWith("/api")) {
+        response.headers.set("Access-Control-Allow-Methods", "GET, POST")
+        response.headers.set("Access-Control-Allow-Origin", "*")
+        response.headers.set("Access-Control-Allow-Headers", "*")
+    }
+
+    if (event.url.pathname.startsWith("/api/auth")) {
+        response.headers.set("Access-Control-Allow-Origin", "https://coppersalts.github.io")
+    }
+
+    return response;
 }) satisfies Handle;
