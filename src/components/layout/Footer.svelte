3470decfc3b8ca4d405af76dca1728d0f5f6c491
Zolo101 <umadeguy@gmail.com>
2024-02-24 00:16:34 +0000

    Fix Footer not always being at the bottom of the page

--- src/components/layout/Footer.svelte
+++ src/components/layout/Footer.svelte
@@ -2,9 +2,9 @@
     import FiveBStyle from "../FiveBStyle.svelte";
 </script>
 
-<div class="flex justify-evenly text-neutral-50 bg-neutral-100/10 p-5">
+<footer>
     <a class="hover:underline" href="https://zelo.dev/" target="_blank">Site by Zelo101</a>
     <div>
         <FiveBStyle text="5beam is not affiliated with Jacknjellify."/>
     </div>
-</div>
+</footer>
