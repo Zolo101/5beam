325fff2003e6f2d7371c8c474b64a123152b42a9
Zelo101 <umadeguy@gmail.com>
2025-03-18 12:07:00 +0000

    Different buttons for different stuff

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
     export let text: string = "(text)";
     export let bg: string = "#ffffff";
-    export let href: string = "#";
-    export let onclick: () => void = () => {};
+    export let href: string = "";
     export let event: string = "";
     export let disabled: boolean = false;
+    export let newWindow: boolean = false;
 </script>
 
-<a {href} on:click={onclick}>
+<a {href} target={newWindow ? "_blank" : "_self"}>
     <button
         {disabled}
         data-umami-event={event}
         class="
-        h-[40px] cursor-pointer text-black transition-all hover:outline-black/50 hover:brightness-75
-        disabled:cursor-not-allowed
-    "
+        h-[40px] cursor-pointer text-black inset-shadow-xs inset-shadow-black/50 transition-all hover:outline-black/50 hover:brightness-75
+        disabled:cursor-not-allowed disabled:opacity-75 disabled:brightness-75
+        "
         style="background-color: {bg}"
     >
         {text}
