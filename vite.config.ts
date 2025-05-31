9ca80b0d0214f3b97a34e9036691be5b94d5bb13
Zelo101 <umadeguy@gmail.com>
2025-05-31 02:07:22 +0100

    Add Vite plugin devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import type { UserConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 
 const config: UserConfig = {
-    plugins: [sveltekit()]
+    plugins: [sveltekit(), devtoolsJson()]
 };
 
 export default config;
