a5cbe002157d7ba35161f8572f983f35eb73689b
Zelo101 <umadeguy@gmail.com>
2025-07-10 01:52:48 +0100

    Move table css to component

--- src/components/layout/Table.svelte
+++ src/components/layout/Table.svelte
@@ -6,23 +6,21 @@
         dynamicWidth?: boolean;
     }
 
-    let {
-        title = "",
-        heads = [],
-        content,
-        dynamicWidth = true
-    }: Props = $props();
+    let { title = "", heads = [], content, dynamicWidth = true }: Props = $props();
 </script>
 
-<table class:dynamicWidth class="mt-5">
+<table
+    class:dynamicWidth
+    class="mt-5 rounded border-hidden text-neutral-200 outline-4 outline-black"
+>
     {#if title}
-        <caption>{title}</caption>
+        <caption class="bg-black p-1 text-sm">{title}</caption>
     {/if}
     {#if heads}
         <thead>
-            <tr>
+            <tr class="bg-neutral-700">
                 {#each heads as head}
-                    <th>{head}</th>
+                    <th class="border-2 border-neutral-800 bg-neutral-700 p-2.5">{head}</th>
                 {/each}
             </tr>
         </thead>
@@ -31,9 +29,25 @@
         {#each content as row}
             <tr>
                 {#each row as item}
-                    <td>{item}</td>
+                    <td class="border-2 border-neutral-800 bg-neutral-600 p-2.5">{item}</td>
                 {/each}
             </tr>
         {/each}
     </tbody>
 </table>
+
+<style>
+    table {
+        background: repeating-linear-gradient(
+            -45deg,
+            #5a5a5a,
+            #5a5a5a 10px,
+            #464646 10px,
+            #464646 20px
+        );
+    }
+
+    .dynamicWidth {
+        width: 100%;
+    }
+</style>
