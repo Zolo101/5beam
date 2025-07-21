d36b71209ff7b63dd625cee8b60da11c1a7b63ce
Zelo101 <umadeguy@gmail.com>
2025-07-21 01:18:02 +0100

    Quick env rename

--- src/lib/adminPocketbase.ts
+++ src/lib/adminPocketbase.ts
@@ -1,6 +1,6 @@
-import { ADMIN_EMAIL, ADMIN_PASSWORD } from "$env/static/private";
+import { ADMIN_EMAIL_PB, ADMIN_PASSWORD_PB } from "$env/static/private";
 import Pocketbase from "pocketbase";
 
 /** Server-side only. Importing this into client-side code will cause an error. */
 export const adminPb = new Pocketbase("https://cdn.zelo.dev");
-adminPb.collection("_superusers").authWithPassword(ADMIN_EMAIL, ADMIN_PASSWORD);
+adminPb.collection("_superusers").authWithPassword(ADMIN_EMAIL_PB, ADMIN_PASSWORD_PB);
