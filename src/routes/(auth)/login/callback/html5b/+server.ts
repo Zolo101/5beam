8f5299635198a3261bc11a5dfa50137289a7e3f5
Zelo101 <umadeguy@gmail.com>
2025-07-21 04:12:28 +0100

    HTML5b use HTML Authorization for Auth

--- src/routes/(auth)/login/callback/html5b/+server.ts
+++ src/routes/(auth)/login/callback/html5b/+server.ts
@@ -9,7 +9,7 @@ export const GET: RequestHandler = async ({ cookies, locals, url }) => {
     const codeVerifier = cookies.get("discord_code_verifier");
     if (!codeVerifier) return redirect(308, "/");
 
-    const { token } = (await locals.pb
+    const { record, token } = (await locals.pb
         .collection("5beam_users")
         .authWithOAuth2Code("discord", code, codeVerifier, redirectURL_html5b)) as {
         meta: DiscordMeta;
@@ -19,11 +19,15 @@ export const GET: RequestHandler = async ({ cookies, locals, url }) => {
 
     cookies.delete("discord_code_verifier", { path: "/" });
 
+    // big thanks to advertisers for ruining third party cookies
+
+    const fivebeam_auth = encodeURI(JSON.stringify({ record, token }));
+
     return new Response("", {
         status: 302,
         headers: {
-            location: `https://coppersalts.github.io/HTML5b/authredirect?5beam_auth=${token}`
-            // location: `http://localhost:8080/authredirect.html?5beam_auth=${token}`
+            // location: `https://coppersalts.github.io/HTML5b/authredirect?5beam_auth=${fivebeam_auth}`
+            location: `http://localhost:8080/authredirect.html?5beam_auth=${fivebeam_auth}`
         }
     });
 };
