656834ef133057404c7fb2c753879d297e2bf66c
Zelo101 <umadeguy@gmail.com>
2025-10-26 22:34:12 +0000

    Change footer text to new logo

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,10 +1,15 @@
 <script>
     import FiveBStyle from "../FiveBStyle.svelte";
+    import ZeloLogoTransparent from "../zelo_logo_transparent.svelte";
 </script>
 
 <footer class="mt-auto flex justify-evenly bg-neutral-700 p-5 text-neutral-50">
     <div>
-        <a class="hover:underline" href="https://zelo.dev/" target="_blank">Site by Zelo101</a>
+        <!-- <span>Site by</span> -->
+        <!-- <span>By</span> -->
+        <a class="relative bottom-0.5 hover:underline" href="https://zelo.dev/" target="_blank"
+            ><ZeloLogoTransparent width="40" height="20" textFill="white" /></a
+        >
         <span>•</span>
         <a class="hover:underline" href="/api"><FiveBStyle text="5beam API" /></a>
     </div>
