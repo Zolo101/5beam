78f2ef1465fb5785af9e00e335b55e1c1fc06330
Zelo101 <umadeguy@gmail.com>
2026-01-01 04:30:25 +0000

    Feature: SVG Components (@poppanator/sveltekit-svg)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import devtoolsJson from "vite-plugin-devtools-json";
+import svg from "@poppanator/sveltekit-svg";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
+    plugins: [
+        tailwindcss(),
+        sveltekit(),
+        devtoolsJson(),
+        svg({
+            svgoOptions: {
+                plugins: [
+                    {
+                        name: "prefixIds"
+                    }
+                ]
+            }
+        })
+    ]
 });
