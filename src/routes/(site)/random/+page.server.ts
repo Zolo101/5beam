0f1a29b457757f136f820075471afca0500b1ca7
Zelo101 <umadeguy@gmail.com>
2026-02-18 01:52:43 +0000

    Fix: /random 400

--- src/routes/(site)/random/+page.server.ts
+++ src/routes/(site)/random/+page.server.ts
@@ -1,8 +1,8 @@
 import type { PageServerLoad } from "./$types";
-import { getRandomLevels } from "$lib/get.remote";
-import type { Level } from "$lib/types";
+// import { getRandomLevels } from "$lib/get.remote";
+// import type { Level } from "$lib/types";
 
 export const load = (async () => {
-    const randomLevels = (await getRandomLevels(12, 0, false, "")) as Level[];
-    return { randomLevels };
+    // const randomLevels = (await getRandomLevels(12, 0, false, "")) as Level[];
+    // return { randomLevels };
 }) satisfies PageServerLoad;
